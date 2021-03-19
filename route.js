9db5a6f657867f0cfb9927403d5deae27bd0c54d
Squirrhell <32261427+Squirrhell@users.noreply.github.com>
2021-03-19 17:53:46 +0100

    add obj router

--- route.js
+++ route.js
@@ -1,69 +1,73 @@
-function startRouter(){
-    let hash = window.location.hash;
-    if(hash != "#home" && hash != "#favorite" && hash != "#deleted"){
-        history.pushState(null, null, "#home");
+class Router {
+
+    startRouter(){
+        let hash = window.location.hash;
+        if(hash != "#home" && hash != "#favorite" && hash != "#deleted"){
+            history.pushState(null, null, "#home");
+        }
+        this.hashChangedRoute();
+    };
+
+    home(view){
+        let div = document.createElement('div');
+        div.id = "divMain";
+
+        let button = document.createElement('button');
+        button.innerHTML = "Roll";
+        button.setAttribute("onclick", "affichageChienRandom()");
+        
+        view.appendChild(div);
+        view.appendChild(button);
+
+        affichageChienRandom();
     }
-    hashChangedRoute();
-};
 
-function home(view){
-    let div = document.createElement('div');
-    div.id = "divMain";
+    favorite(view){
+        let div = document.createElement('div');
+        div.id = "divMain";
+        
+        view.appendChild(div);
 
-    let button = document.createElement('button');
-    button.innerHTML = "Roll";
-    button.setAttribute("onclick", "affichageChienRandom()");
-    
-    view.appendChild(div);
-    view.appendChild(button);
+        readDogo(1);
+    };
 
-    affichageChienRandom();
+    deleted(view){
+        let div = document.createElement('div');
+        div.id = "divMain";
+        
+        view.appendChild(div);
+
+        readDogo(2);
+    };
+
+
+
+    hashChangedRoute() {
+        const hash = window.location.hash;  
+        console.log(hash);
+        const view = document.getElementById("view");
+        view.innerHTML = "";
+
+        switch (hash) {
+            case "#home":
+                router.home(view);
+            break;
+
+            case "#favorite":
+                router.favorite(view);
+            break;
+
+            case "#deleted":
+                router.deleted(view);
+            break;
+
+            default:
+                view.innerHTML = "<h1>404 - Page Not Found</h1>";
+            break;
+        } 
+    }  
 }
 
-function favorite(view){
-    let div = document.createElement('div');
-    div.id = "divMain";
-    
-    view.appendChild(div);
-
-    readDogo(1);
-};
-
-function deleted(view){
-    let div = document.createElement('div');
-    div.id = "divMain";
-    
-    view.appendChild(div);
-
-    readDogo(2);
-};
-
-
-
-function hashChangedRoute() {
-    const hash = window.location.hash;  
-    console.log(hash);
-    const view = document.getElementById("view");
-    view.innerHTML = "";
-
-    switch (hash) {
-        case "#home":
-            home(view);
-        break;
-
-        case "#favorite":
-            favorite(view);
-        break;
-
-        case "#deleted":
-            deleted(view);
-        break;
-
-        default:
-            view.innerHTML = "<h1>404 - Page Not Found</h1>";
-        break;
-    } 
-}  
-
-window.addEventListener("hashchange", hashChangedRoute);
+let router = new Router();
+window.addEventListener("hashchange", router.hashChangedRoute);
 
